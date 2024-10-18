6f5843f017c08b57d6388dd426bd6269985f0b9f
nicry do bota <111884993+nicolasntr10@users.noreply.github.com>
2024-10-17 21:17:40 -0300

    n

--- informacoesGlobais.js
+++
@@ -1,8 +0,0 @@
-const url="https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/transporte/transporte-dados-globais.json"
-
-async function visualizarInformaçõesGlobais(){
-    constant resposta = await fetch (url)
-    let dados = await resposta.json;
-    console.log( dados );
-}
-visualizarInformaçõesGlobais();
